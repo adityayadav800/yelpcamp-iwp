e9bb6176438eac3741dea12cd10d6487bc248456
adityayadav800 <cooladityaamo@gmail.com>
2020-10-18 11:07:09 +0530

    Added PHP

---
+++ views/index.php
@@ -0,0 +1,122 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<script type="text/javascript">
+setTimeout(function() {
+  $('#loading').addClass('hidden');
+}, 3000);
+</script>
+<style type="text/css">
+#loading{
+  position:fixed;
+  top:0;
+  left:0;
+  right:0;
+  bottom:0;
+  background-color: gray;
+  color: white;
+  opacity: 1;
+  transition: 0.5s;
+  visibility: visible;
+}
+#loading.hidden{
+  opacity: 0;
+  visibility: hidden;
+}
+</style>
+<div id="loading"></div>
+<div id="site">
+</div>
+
+
+<?php 
+
+if(isset($_POST['submit'])){
+
+
+
+    $data=array();
+    $data['body']=array();
+    $data['body']['name']=$_POST['name'];
+    $data['body']['image']=$_POST['image'];
+    $data['body']['description']=$_POST['description'];
+    $data['body']['location']=$_POST['location'];
+    $data['user']=array();
+    $data['user']['_id']=$_POST['user']['_id'];
+    $data['user']['username']=$_POST['user']['username'];
+
+    $dataTOjson=json_encode($data);
+
+    $response = HTTPRequester::HTTPPost("/routes/campgrounds", $data);
+}
+    class HTTPRequester {
+    /**
+     * @description Make HTTP-GET call
+     * @param       $url
+     * @param       array $params
+     * @return      HTTP-Response body or an empty string if the request fails or is empty
+     */
+    public static function HTTPGet($url, array $params) {
+        $query = http_build_query($params); 
+        $ch    = curl_init($url.'?'.$query);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        $response = curl_exec($ch);
+        curl_close($ch);
+        return $response;
+    }
+    /**
+     * @description Make HTTP-POST call
+     * @param       $url
+     * @param       array $params
+     * @return      HTTP-Response body or an empty string if the request fails or is empty
+     */
+    public static function HTTPPost($url, array $params) {
+        $query = http_build_query($params);
+        $ch    = curl_init();
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $query);
+        $response = curl_exec($ch);
+        curl_close($ch);
+        return $response;
+    }
+    /**
+     * @description Make HTTP-PUT call
+     * @param       $url
+     * @param       array $params
+     * @return      HTTP-Response body or an empty string if the request fails or is empty
+     */
+    public static function HTTPPut($url, array $params) {
+        $query = \http_build_query($params);
+        $ch    = \curl_init();
+        \curl_setopt($ch, \CURLOPT_RETURNTRANSFER, true);
+        \curl_setopt($ch, \CURLOPT_HEADER, false);
+        \curl_setopt($ch, \CURLOPT_URL, $url);
+        \curl_setopt($ch, \CURLOPT_CUSTOMREQUEST, 'PUT');
+        \curl_setopt($ch, \CURLOPT_POSTFIELDS, $query);
+        $response = \curl_exec($ch);
+        \curl_close($ch);
+        return $response;
+    }
+    /**
+     * @category Make HTTP-DELETE call
+     * @param    $url
+     * @param    array $params
+     * @return   HTTP-Response body or an empty string if the request fails or is empty
+     */
+    public static function HTTPDelete($url, array $params) {
+        $query = \http_build_query($params);
+        $ch    = \curl_init();
+        \curl_setopt($ch, \CURLOPT_RETURNTRANSFER, true);
+        \curl_setopt($ch, \CURLOPT_HEADER, false);
+        \curl_setopt($ch, \CURLOPT_URL, $url);
+        \curl_setopt($ch, \CURLOPT_CUSTOMREQUEST, 'DELETE');
+        \curl_setopt($ch, \CURLOPT_POSTFIELDS, $query);
+        $response = \curl_exec($ch);
+        \curl_close($ch);
+        return $response;
+    }
+}
+
+?>
